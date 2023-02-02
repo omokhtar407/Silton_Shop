9c493f4bc3e819fbe889e41429bee36111d88260
omokhtar407 <omokhtar407@gmail.com>
2023-02-02 17:11:27 +0200

    Update Cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -2,40 +2,80 @@ import { Component, OnInit } from '@angular/core';
 import { Product1 } from 'src/model/product';
 import { CartServicesService } from './../../services/cart-services.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { sweetAlertSuccess } from 'src/sweetalert';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  products:Product1[]= [];
-  grandTotal:number = 0;
+  products: Product1[] = [];
+  grandTotal: number = 0;
 
-  constructor(private _CartService:CartServicesService,private _NgxSpinnerService: NgxSpinnerService) { }
+  constructor(
+    private _CartService: CartServicesService,
+    private _NgxSpinnerService: NgxSpinnerService
+  ) {}
 
-  removeProduct(pro:Product1){
-    this._CartService.removeCartItem(pro)
+  removeProduct(pro: Product1) {
+    this._CartService.removeCartItem(pro);
   }
 
-  removeAllProducts(){
-    this._CartService.removeAllCartItems()
+  removeAllProducts() {
+    this._CartService.removeAllCartItems();
   }
 
+  plusProduct(pro_id: any) {
+    this.products[pro_id].quantity++;
+    this.getTotalPri();
+    localStorage.setItem('cart', JSON.stringify(this.products));
+  }
+  minsProduct(pro_id: any) {
+    this.products[pro_id].quantity--;
+    this.getTotalPri();
+    localStorage.setItem('cart', JSON.stringify(this.products));
+  }
+  detectProQ() {
+    this.getTotalPri();
+    localStorage.setItem('cart', JSON.stringify(this.products));
+  }
 
+  getTotalPri() {
+    this.grandTotal = this._CartService.getTotalPrice();
+  }
+
+  addCart() {
+    let products = this.products.map((pro: Product1) => {
+      return {
+        productId: pro.id,
+        quantity: pro.quantity,
+      };
+    });
+    let model = {
+      userId: 5,
+      date: new Date(),
+      products: products,
+    };
+
+    this._CartService.addNewCart(model).subscribe((res: any) => {
+      if(res){
+        sweetAlertSuccess('Well Done! your order is Successfully send');
+      }
+    });
+  }
   ngOnInit(): void {
     this._NgxSpinnerService.show();
     this._CartService.getProducts().subscribe(
-      (res)=>{
-        setTimeout(()=>{
+      (res) => {
+        setTimeout(() => {
           this._NgxSpinnerService.hide();
-        },2000);
+        }, 2000);
         this.products = res;
-        this.grandTotal = this._CartService.getTotalPrice();
+        this.getTotalPri();
       },
-      (error)=>{
-        console.log("Error: " + error)
+      (error) => {
+        console.log('Error: ' + error);
       }
-    )
+    );
   }
-
 }
--- src/app/services/cart-services.service.ts
+++ src/app/services/cart-services.service.ts
@@ -1,8 +1,10 @@
-import { sweetAlertSuccess } from 'src/sweetalert';
+import { HttpClient } from '@angular/common/http';
+import { sweetAlertError, sweetAlertSuccess } from 'src/sweetalert';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Product1 } from 'src/model/product';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +13,7 @@ export class CartServicesService {
   cartItemList: Product1[] = [];
   productList = new BehaviorSubject<any>([]);
 
-  constructor(private _Router: Router) {
+  constructor(private _Router: Router,private _http:HttpClient) {
     if (localStorage.getItem('cart') != null) {
       this.cartItemList = JSON.parse(localStorage.getItem('cart') as any) || [];
       this.productList.next(this.cartItemList);
@@ -30,45 +32,35 @@ export class CartServicesService {
   addToCart(pro:any){
     if (localStorage.getItem('userToken') != null) {
       if(this.cartItemList.length){
+        // let x = this.cartItemList.findIndex(pr => pr.id === pro.id) > -1; another technic
+        let proExist = this.cartItemList.find(pr => pr.id === pro.id);
 
-          let x = this.cartItemList.findIndex(pr => pr.id === pro.id) > -1;
-
-          if(x){
-            pro.quantity += 1
+          if(proExist){
+            sweetAlertError('Product Already Exist in Cart');
           }
           else{
             this.cartItemList.push(pro);
+            sweetAlertSuccess('Product Added to Cart');
           }
       }
       else{
         this.cartItemList.push(pro);
+        sweetAlertSuccess('Product Added to Cart');
       }
       localStorage.setItem('cart',JSON.stringify(this.cartItemList));
       this.productList.next(this.cartItemList);
       this.getTotalPrice();
-      sweetAlertSuccess('Product Added to Cart');
+
     }
     else{
       this._Router.navigate(['login']);
     }
   }
 
-  // addToCart(...pro: any) {
-  //   if (localStorage.getItem('userToken') != null) {
-  //     this.cartItemList.push(...pro);
-  //     localStorage.setItem('cart', JSON.stringify(this.cartItemList));
-  //     this.productList.next(this.cartItemList);
-  //     this.getTotalPrice();
-  //     sweetAlertSuccess('Product Added to Cart');
-  //   } else {
-  //     this._Router.navigate(['login']);
-  //   }
-  // }
-
   getTotalPrice(): number {
     let grandTotal = 0;
     this.cartItemList.map((pro: Product1) => {
-      grandTotal += pro.total;
+      grandTotal += pro.total * pro.quantity;
     });
 
     return grandTotal;
@@ -90,5 +82,9 @@ export class CartServicesService {
     localStorage.setItem('cart', JSON.stringify(this.cartItemList));
     this.productList.next(this.cartItemList);
     sweetAlertSuccess('All Products Removed from Cart');
+  }
+
+  addNewCart(payload:any){
+    return this._http.post(environment.baseUrl.replace(`products`,`carts`),payload)
   }
 }
